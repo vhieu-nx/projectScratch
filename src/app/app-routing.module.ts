aaf8642277a7f5a2e29512db9eaf748a8cbeb76d
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-07-15 15:27:35 +0700

    project Scratch done using framework Angular 12

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {DetailsComponent} from "./components/details/details.component";
+// import {HomeComponent} from "./home/home.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component:HomeComponent,
+  },
+  {
+    path: 'search/:game-search',
+    component:HomeComponent,
+  },
+  {
+    path: 'details/:id',
+    component: DetailsComponent,
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
